ee29c668674aaa55f6593151091a2b78b650cd7f
Miftahul Zannat Atiha <miftahul.z800@gmail.com>
2026-01-21 19:12:23 +0600

    Initial commit

--- contactform.js
+++ contactform.js
@@ -1,5 +1,10 @@
-const form = document.getElementById("contactForm");
+console.log("Contact form JS loaded");
 
+const form = document.getElementById("contactForm");
+if (!form) {
+  console.error("Form not found!");
+}
+else {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -9,7 +14,7 @@ form.addEventListener("submit", (e) => {
     message: form.message.value
   };
 
-  const webAppURL = "https://script.google.com/macros/s/AKfycbxDfv2usUJddge2M-weam5IG4S_ApaAZOEANYLH97OHtBw9ETf4mhWAWuaQqZoeK-e6/exec";
+  const webAppURL = "https://script.google.com/macros/s/AKfycbwhVU_eI5N9sRbOpFEZLxq1-CFuh4d0kC-S_q2Qpb5HHJmvFs0E_-flu-fRNw0W7DU/exec";
 
   fetch(webAppURL, {
     method: "POST",
@@ -18,13 +23,20 @@ form.addEventListener("submit", (e) => {
     },
     body: JSON.stringify(data)
   })
-  .then(res => res.json())
-  .then(result => {
-    alert("✅ Message sent successfully!");
-    form.reset();
-  })
-  .catch(err => {
-    console.error(err);
-    alert("❌ Failed to send message");
-  });
+    .then(res => res.json())
+    .then(result => {
+      console.log(result);
+
+      if (result.success) {
+        alert("✅ Message sent successfully!");
+        form.reset();
+      } else {
+        alert("❌ Server error");
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      alert("❌ Network error");
+    });
 });
+}
