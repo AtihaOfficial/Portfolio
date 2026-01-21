77daffa2507fe8712bf0937570dd2cd70bf516cf
Miftahul Zannat Atiha <miftahul.z800@gmail.com>
2026-01-21 17:17:21 +0600

    Update contact form js

--- contactform.js
+++ contactform.js
@@ -1,36 +1,30 @@
 const form = document.getElementById("contactForm");
-const msg = document.getElementById("formMsg");
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyrvCNS3ZjOg6gZMMWwEkWXHfO-F-80NdegWF8geVd9-mitmhtL0EvBMhBBaMpNbZqi/exec";
 
-form.addEventListener("submit",function (e) {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const data = {
-    name:form.name.value,
-    email:form.email.value,
-    message:form.message.value
+    name: form.name.value,
+    email: form.email.value,
+    message: form.message.value
   };
 
-  fetch(SCRIPT_URL,{
-    method:"POST",
-    body:JSON.stringify(data),
-    headers:{
-      "Content-Type":"application/json"
-    }
+  const webAppURL = "https://script.google.com/macros/s/AKfycbxDfv2usUJddge2M-weam5IG4S_ApaAZOEANYLH97OHtBw9ETf4mhWAWuaQqZoeK-e6/exec";
+
+  fetch(webAppURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
   })
-
-  .then(response => response.json())
-.then(result => {
-  if (result.result === "success") {
-    msg.textContent = "Message sent successfully!";
-    msg.style.color = "lightgreen";
+  .then(res => res.json())
+  .then(result => {
+    alert("✅ Message sent successfully!");
     form.reset();
-  } else {
-    throw new Error("Server error");
-  }
-})
-.catch(() => {
-  msg.textContent = "Something went wrong!";
-  msg.style.color = "red";
+  })
+  .catch(err => {
+    console.error(err);
+    alert("❌ Failed to send message");
+  });
 });
-
