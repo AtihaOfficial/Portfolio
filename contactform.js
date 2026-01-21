83dd220e7934cf46e0a750a495f761dd3ddfa76b
Miftahul Zannat Atiha <miftahul.z800@gmail.com>
2026-01-21 23:29:57 +0600

    Portfolio site

--- contactform.js
+++ contactform.js
@@ -1,42 +1,40 @@
 console.log("Contact form JS loaded");
-
 const form = document.getElementById("contactForm");
+const formMsg = document.getElementById("formMsg");
 if (!form) {
   console.error("Form not found!");
-}
-else {
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+} else {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formMsg.textContent = "Sending...";
+    formMsg.style.color = "#0066cc";
+    const data = {
+      name: form.name.value,
+      email: form.email.value,
+      message: form.message.value
+    };
 
-  const data = {
-    name: form.name.value,
-    email: form.email.value,
-    message: form.message.value
-  };
 
-  const webAppURL = "https://script.google.com/macros/s/AKfycbwhVU_eI5N9sRbOpFEZLxq1-CFuh4d0kC-S_q2Qpb5HHJmvFs0E_-flu-fRNw0W7DU/exec";
+    
+    const webAppURL = "https://script.google.com/macros/s/AKfycbz73nq8U53QMpaooUtDGaOgWEFHATSPCZNDgR5r14WfBXbFsrQJRl0e-VC6e1oIYCof/exec";
 
-  fetch(webAppURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-    .then(res => res.json())
-    .then(result => {
-      console.log(result);
-
-      if (result.success) {
-        alert("✅ Message sent successfully!");
-        form.reset();
-      } else {
-        alert("❌ Server error");
-      }
+    fetch(webAppURL, {
+      method: "POST",
+      mode: "no-cors",  
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
     })
-    .catch(err => {
-      console.error(err);
-      alert("❌ Network error");
-    });
-});
+      .then(() => {
+        formMsg.textContent = "✅ Message sent successfully!";
+        formMsg.style.color = "#28a745";
+        form.reset();
+      })
+      .catch(err => {
+        console.error(err);
+        formMsg.textContent = "❌ Failed to send. Please try again.";
+        formMsg.style.color = "#dc3545";
+      });
+  });
 }
